10950f4bb5ea762bc6a67f0d96003d6c439241c5
VK <abdu081680@gmail.com>
2025-04-22 14:58:41 -0400

    Removed block for pc in some front pages

--- resources/views/backend/pages/visa/show.blade.php
+++ resources/views/backend/pages/visa/show.blade.php
@@ -11,6 +11,33 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
 
+    <style>
+    .main-file .div-3 {
+        /* top: 17px;
+        left: 36px;
+        font-family: "Jali Arabic-Bold";
+        font-weight: 900;
+        color: #121619;
+        font-size: 14px;
+        white-space: nowrap;
+        position: absolute;
+        direction: rtl; */
+
+        
+        width: 200px;
+        top: 17px;
+        left: -50px;
+        font-family: "Jali Arabic-Bold";
+        font-weight: 900;
+        color: #121619;
+        font-size: 14px;
+        text-align: right;
+        line-height: normal;
+        white-space: nowrap;
+        position: absolute;
+        direction: rtl;
+    }
+</style>
 
 </head>
 
--- resources/views/frontend/pages/evisa-find-download_user_form.blade.php
+++ resources/views/frontend/pages/evisa-find-download_user_form.blade.php
@@ -252,50 +252,5 @@
 
             document.addEventListener("DOMContentLoaded", loadNationalities);
         </script>
-        <script>
-            const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-          
-            if (!isMobile) {
-              document.head.innerHTML += `
-                <style>
-                  body {
-                    margin: 0;
-                    padding: 0;
-                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                    background: #f4f4f4;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    height: 100vh;
-                    text-align: center;
-                  }
-          
-                  .mobile-only-wrapper {
-                    background: white;
-                    padding: 2rem 3rem;
-                    border: 1px solid #ddd;
-                    border-radius: 10px;
-                    box-shadow: 0 10px 25px rgba(0,0,0,0.1);
-                  }
-          
-                  .mobile-only-wrapper h3 {
-                    color: #ff3e3e;
-                    margin-bottom: 0.5rem;
-                  }
-          
-                  .mobile-only-wrapper p {
-                    color: #333;
-                  }
-                </style>
-              `;
-          
-              document.body.innerHTML = `
-                <div class="mobile-only-wrapper">
-                  <h3>Mobile Device Required</h3>
-                  <p>This app is only accessible on smartphones or tablets.</p>
-                </div>
-              `;
-            }
-          </script>
     @endpush
 @endsection
