21a16a739156496c469da157b06e1e25a63c770e
VK <abdu081680@gmail.com>
2025-04-24 11:16:32 -0400

    Done

--- resources/views/frontend/pages/evisa_web_app_details.blade.php
+++ resources/views/frontend/pages/evisa_web_app_details.blade.php
@@ -74,12 +74,12 @@
         .row {
             display: flex;
             justify-content: space-between;
-            margin: 0.5rem 0;
+            margin: 0.1rem 0;
         }
 
         .row2 {
             display: flex;
-            margin: 0.5rem 0;
+            margin: 0.1rem 0;
         }
 
         .label {
@@ -266,7 +266,7 @@
         Visa Details
     </div>
 
-    <div class="flex text-white" style="background: #36456e; font-size: 0.7rem; font-weight: 800; letter-spacing: 0.03rem;">
+    <div class="flex text-white" style="background: #36456e; font-size: 0.7rem; font-weight: 800;">
         <button onclick="showTab('details')" id="tab-details"
             class="flex-1 text-center py-3 px-4 flex items-center justify-center gap-2 border-b-2 border-transparent hover:border-blue-400 focus:outline-none">
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
@@ -278,10 +278,12 @@
 
         <button onclick="showTab('qr')" id="tab-qr"
             class="flex-1 text-center py-3 px-4 flex items-center justify-center gap-2 border-b-2 border-transparent hover:border-blue-400 focus:outline-none">
-            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+            <svg class="w-5 h-5" style="margin-right: -22px;" fill="currentColor" viewBox="0 0 24 24">
                 <path
                     d="M3 3h6v6H3V3zm2 2v2h2V5H5zm10-2h6v6h-6V3zm2 2v2h2V5h-2zM3 15h6v6H3v-6zm2 2v2h2v-2H5zm10 0h2v2h-2v-2zm-2-4h2v2h-2v-2zm4 0h2v2h-2v-2zm2 2h2v2h-2v-2zm-6 4h2v2h-2v-2zm2 2h2v2h-2v-2zm2-2h2v2h-2v-2z" />
             </svg>
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>ionicons-v5-k</title><rect x="336" y="336" width="80" height="80" rx="8" ry="8"></rect><rect x="272" y="272" width="64" height="64" rx="8" ry="8"></rect><rect x="416" y="416" width="64" height="64" rx="8" ry="8"></rect><rect x="432" y="272" width="48" height="48" rx="8" ry="8"></rect><rect x="272" y="432" width="48" height="48" rx="8" ry="8"></rect><path d="M448,32H304a32,32,0,0,0-32,32V208a32,32,0,0,0,32,32H448a32,32,0,0,0,32-32V64A32,32,0,0,0,448,32ZM416,168a8,8,0,0,1-8,8H344a8,8,0,0,1-8-8V104a8,8,0,0,1,8-8h64a8,8,0,0,1,8,8Z"></path><path d="M208,32H64A32,32,0,0,0,32,64V208a32,32,0,0,0,32,32H208a32,32,0,0,0,32-32V64A32,32,0,0,0,208,32ZM176,168a8,8,0,0,1-8,8H104a8,8,0,0,1-8-8V104a8,8,0,0,1,8-8h64a8,8,0,0,1,8,8Z"></path><path d="M208,272H64a32,32,0,0,0-32,32V448a32,32,0,0,0,32,32H208a32,32,0,0,0,32-32V304A32,32,0,0,0,208,272ZM176,408a8,8,0,0,1-8,8H104a8,8,0,0,1-8-8V344a8,8,0,0,1,8-8h64a8,8,0,0,1,8,8Z"></path></g></svg>
+
             <span>QR Code</span>
         </button>
     </div>
@@ -319,7 +321,7 @@
                 </div>
             </div>
 
-            <div class="row2" style="margin-top: 1rem;">
+            <div class="row2" style="margin-top: 0.7rem;">
                 <div class="label2">Visa Type:</div>
                 <div class="value2" style="color: #3567b6; font-weight: 700;
         margin-left: -80px;">
@@ -335,7 +337,7 @@
                 <div class="value2 value-b" style="margin-left: -54px;">{{ $evisaApps->place_of_issue }}</div>
             </div>
 
-            <div class="row" style="margin-top: 24px">
+            <div class="row" style="margin-top: 0.8rem">
                 <div class="label">Date of Issue</div>
                 <div class="label" style="text-align: end;">Date of Expiry</div>
             </div>
